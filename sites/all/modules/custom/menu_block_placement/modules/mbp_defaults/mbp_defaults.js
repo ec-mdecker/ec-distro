70905634e64639a5df6904fd25b5723707d85164
Mike Decker <mdecker@eaglecrk.com>
2014-10-06 14:02:40 -0500

    added weight:10 to blocks

--- sites/all/modules/custom/menu_block_placement/modules/mbp_defaults/mbp_defaults.js
+++ sites/all/modules/custom/menu_block_placement/modules/mbp_defaults/mbp_defaults.js
@@ -3,7 +3,6 @@ jQuery(document).ready(function ($) {
   $("[id^=block-mbp-defaults-mbp-]").each(function () {
     var links = $(this).find('ul.quicktabs-tabs').contents();
     $(this).find('.contextual-links-wrapper:first ul').append(links);
-    $(this).find('.contextual-links-wrapper:first').css('right', '50px');
     $(this).find('h2.block-title:first').remove();
     $(this).find('ul.quicktabs-tabs').remove();
     $(this).find('li.block-configure').remove();
