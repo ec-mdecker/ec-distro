a68d06c3d449ebe70f5517dfde151f94ad80a293
Mike Decker <mdecker@eaglecrk.com>
2014-10-07 17:26:07 -0500

    cleaned up features strong arm settings

--- sites/all/modules/custom/menu_block_placement/modules/mbp_defaults/mbp_defaults.js
+++ sites/all/modules/custom/menu_block_placement/modules/mbp_defaults/mbp_defaults.js
@@ -1,10 +1,17 @@
 //moves the quicktabs links into the contextual links
-jQuery(document).ready(function ($) {
-  $("[id^=block-mbp-defaults-mbp-]").each(function () {
-    var links = $(this).find('ul.quicktabs-tabs').contents();
-    $(this).find('.contextual-links-wrapper:first ul').append(links);
-    $(this).find('h2.block-title:first').remove();
-    $(this).find('ul.quicktabs-tabs').remove();
-    $(this).find('li.block-configure').remove();
-  });
-});
+(function ($) {
+  Drupal.behaviors.mbp_defaults = {
+    'attach': function (context) {
+      $(document).ready(function () {
+        $("[id^=block-mbp-defaults-mbp-]").each(function () {
+          var links = $(this).find('ul.quicktabs-tabs').contents();
+          $(this).find('.contextual-links-wrapper:first ul').append(links);
+          $(this).find('h2.block-title:first').remove();
+          $(this).find('ul.quicktabs-tabs').remove();
+          $(this).find('li.block-configure').remove();
+        });
+      })
+
+    }
+  }
+})(jQuery);
